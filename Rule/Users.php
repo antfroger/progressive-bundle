73a97e59b3500c5be98c2a14cfdb0a8945995dc0
Antoine Froger <antfroger@gmail.com>
2021-01-12 11:44:50 +0100

    Add the Symfony specic rule `users`
    This rule enables a feature depending on the user username

---
+++ Rule/Users.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Af\ProgressiveBundle\Rule;
+
+use Progressive\ParameterBagInterface;
+use Progressive\Rule\RuleInterface;
+use Symfony\Component\Security\Core\Security;
+
+class Users implements RuleInterface
+{
+    /** @var Security */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function decide(ParameterBagInterface $bag, array $usernames = []): bool
+    {
+        if (null === $user = $this->security->getUser()) {
+            return false;
+        }
+
+        return in_array($user->getUsername(), $usernames);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'users';
+    }
+}
